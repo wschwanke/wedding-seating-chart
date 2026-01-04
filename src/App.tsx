e9b6e6a13ba5d855cc00e1fbae17845249f78154
Will Schwanke <wschwanke@gmail.com>
2026-01-04 09:13:57 -0600

    Move project to src directory

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <main className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold">Wedding Seating Chart</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
